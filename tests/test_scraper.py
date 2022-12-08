218a366121ce51884d4b1381b429d67baf3c42a1
Leo Kim <leonardkim96@gmail.com>
2022-12-08 00:10:00 -0500

    updated tests to match new api

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -7,15 +7,15 @@ def test_valid_name_no_location(scraper):
     scraper.name = "Aissatou-Ba"
     scraper.city = ""
     scraper.state = ""
-    results = scraper.get_results()
+    results = scraper.get_parsed_results(scraper.get_raw_results())
     assert len(results) > 0
 
 
 def test_valid_name_valid_city(scraper):
     scraper.name = "Aissatou-Ba"
-    scraper.city = "New York"
+    scraper.city = "Columbus"
     scraper.state = ""
-    results = scraper.get_results()
+    results = scraper.get_parsed_results(scraper.get_raw_results())
     assert len(results) > 0
 
 
@@ -23,15 +23,15 @@ def test_valid_name_valid_state(scraper):
     scraper.name = "Aissatou-Ba"
     scraper.city = ""
     scraper.state = "New York"
-    results = scraper.get_results()
+    results = scraper.get_parsed_results(scraper.get_raw_results())
     assert len(results) > 0
 
 
 def test_valid_name_valid_city_and_state(scraper):
     scraper.name = "Aissatou-Ba"
-    scraper.city = "New York"
-    scraper.state = "New York"
-    results = scraper.get_results()
+    scraper.city = "Memphis"
+    scraper.state = "Tennessee"
+    results = scraper.get_parsed_results(scraper.get_raw_results())
     assert len(results) > 0
 
 
@@ -40,16 +40,16 @@ def test_invalid_name(scraper):
     scraper.name = "kajsdhsada"
     scraper.city = ""
     scraper.state = ""
-    results = scraper.get_results()
-    assert len(results) == 0
+    results = scraper.get_raw_results()
+    assert results == ""
 
 
 def test_valid_name_invalid_city(scraper):
     scraper.name = "Aissatou-Ba"
     scraper.city = "kajsdhajksdh"
     scraper.state = ""
-    results = scraper.get_results()
-    assert len(results) == 0
+    results = scraper.get_raw_results()
+    assert results == ""
 
 
 def test_valid_name_invalid_state(scraper):
@@ -57,5 +57,5 @@ def test_valid_name_invalid_state(scraper):
     scraper.city = ""
     scraper.state = "kajsdhajksdh"
 
-    results = scraper.get_results()
-    assert len(results) == 0
+    results = scraper.get_raw_results()
+    assert results == ""
